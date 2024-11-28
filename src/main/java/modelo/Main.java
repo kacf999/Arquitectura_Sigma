9a86f0c509c6f8dede57c47b820d97ed30088894
Paradox <carlos.ascencio@alumnos.uacm.edu.mx>
2024-11-28 11:12:17 -0600

    Solo se agregaron pruebas

--- src/main/java/modelo/Main.java
+++ src/main/java/modelo/Main.java
@@ -1,7 +1,9 @@
 package modelo;
 
+import java.sql.Date;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import conexion.ClienteDAO;
 
@@ -9,19 +11,53 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException, ExceptionPoliza, ParseException {
 		ClienteDAO clienteDAO=new ClienteDAO();
-		Cliente c1=new Cliente("Armando", "Hoyos", "Eloigameno", "1999-12-28","Siempre Viva", "AENCE654848E");
-		Cliente c2=new Cliente("Pedro", "Picapiedra", "Troncoso", "2005-05-31","Caverna 55", "PEPT");
-		Cliente c3=new Cliente("Lola", "Trailera", "Saavedra", "2000-01-15","Siempre Viva", "LOLTE654848E");
+//		Cliente c1=new Cliente("Armando", "Hoyos", "Eloigameno", "2005/12/28","Siempre Viva", "AENCE654848E");
+//		Cliente c2=new Cliente("Pedro", "Picapiedra", "Troncoso", "0500/05/31","Caverna 55", "PEPT");
+//		Cliente c4=new Cliente("Vilma", "Picapiedra", "Troncoso", "0500/10/28","Caverna 55", "AENCE6548E");
+//		Cliente c3=new Cliente("Lola", "Trailera", "Saavedra", "2000/01/15","Siempre Viva", "LOLTE654848E");
+//		
+//		if(clienteDAO.updateCliente(c1)) {
+//			System.out.println("Exito");
+//		}else {
+//			System.out.println("NO Exito");
+//		}
+//
+//		if(clienteDAO.updateCliente(c2)) {
+//			System.out.println("Exito");
+//		}else {
+//			System.out.println("NO Exito");
+//		}
+//		
+//		if(clienteDAO.updateCliente(c4)) {
+//			System.out.println("Exito");
+//		}else {
+//			System.out.println("NO Exito");
+//		}
+			
+//		c1.setFechaNacimientoString("1998/09/10");
 		
-		Cliente cB=clienteDAO.getCliente("PEPT");
+		
+//		System.out.println(c1.getCurp());
+//		System.out.println(c1.getDireccion());
+//		System.out.println(c1.getNombre());
+//		System.out.println(c1.getPrimerApellido());
+//		System.out.println(c1.getSegundoApellido());
+//		System.out.println(c1.getFechaNacimientoDate());
+
+		
+		
+		Cliente cB=clienteDAO.getCliente("AENCE6548E");
 		
 		System.out.println(cB.getCurp());
 		System.out.println(cB.getDireccion());
 		System.out.println(cB.getNombre());
 		System.out.println(cB.getPrimerApellido());
 		System.out.println(cB.getSegundoApellido());
-		System.out.println(cB.getFechaNacimientoString());
+		System.out.println(cB.getFechaNacimientoDate());
+
 		
+		
+
 //		clienteDAO.addCliente(c1);
 //		clienteDAO.addCliente(c2);
 //		clienteDAO.addCliente(c3);
