4cce01cd12500ba8bfa72fac0a35c2768ed42103
Paradox <carlos.ascencio@alumnos.uacm.edu.mx>
2024-12-06 02:06:25 -0600

    Se agrego el atributo "curp_cliente" con su respectiva validacion para poder ser usada correctamente con la clase DAO.

--- src/main/java/mx/edu/uacm/is/stl/as/ws/modelo/Poliza.java
+++ src/main/java/mx/edu/uacm/is/stl/as/ws/modelo/Poliza.java
@@ -52,7 +52,6 @@ public class Poliza {
 	
 	public boolean setCurpCliente(String curp) throws ExceptionPoliza {
 		boolean exito=false;
-//		String regexCURP = "[\\A-Z]{4}[0-9]{6}[HM]{1}[A-Z]{2}[BCDFGHJKLMNPQRSTVWXYZ]{3}([A-Z]{2})?([0-9]{2})?/g";
 		Pattern pattern = Pattern.compile("[A-Z]{4}[0-9]{6}[HM]{1}[A-Z]{2}[BCDFGHJKLMNPQRSTVWXYZ]{3}([A-Z]{2})?([0-9]{2})");
 		
 		Matcher matcher = pattern.matcher(curp);
